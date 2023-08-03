ae09b6df9394c028f5708b08b4f757bf85b37f89
GethinDavies1990 <gethindavies1990@gmail.com>
2023-08-03 20:05:34 +0000

    react-practise

--- vite-react/src/components/Main.jsx
+++ vite-react/src/components/Main.jsx
@@ -1,7 +1,18 @@
 function Main() {
-    return (
-        <h1>Main</h1>
-    )
+  return (
+    <div>
+      <main>
+        <h1 className="main--title">Fun facts about React</h1>
+        <ul className="main--facts">
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers Thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
