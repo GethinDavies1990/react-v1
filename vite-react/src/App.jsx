f0701eab096d36b4fd28f5e63cad474f125535ef
GethinDavies1990 <gethindavies1990@gmail.com>
2023-08-03 20:57:24 +0000

    react playround

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,14 +1,12 @@
-import './App.css'
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
+import React from "react"
 
-function App() {
-  return (
-    <div className='container'>
-      <Navbar />
-      <Main />
-    </div>
-  )
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+        <Navbar />
+    )
 }
-
-export default App
--- vite-react/src/components/Navbar.jsx
+++ vite-react/src/components/Navbar.jsx
@@ -1,11 +1,5 @@
 function Navbar() {
     return (
-        <nav>
-            <img src="../images/react-logo.png" className="nav--icon" />
-            <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
-        </nav>
+        <img src="./images/airbnb 1.png" />
     )
 }
-
-export default Navbar
