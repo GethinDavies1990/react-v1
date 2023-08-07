7d1f97ed08774ff0895e94415be39bafce34622c
GethinDavies1990 <gethindavies1990@gmail.com>
2023-08-07 20:35:58 +0000

    add new air bnb cloner

--- vite-react/src/components/Card.jsx
+++ vite-react/src/components/Card.jsx
@@ -1,15 +1,15 @@
-function Card() {
+function Card(props) {
   return (
     <div className="card">
-      <img src="../images/card-image.png" className="card--image" />
+      <img src={`../images/${props.img}`}/>
       <div className="card--stats">
         <img src="../images/star.png" className="card--star"/>
-        <span>5.0</span>
-        <span className="grey">(6) • </span>
-        <span className="grey">USA</span>
+        <span>{props.rating}</span>
+        <span className="grey">({props.reviewCount}) • </span>
+        <span className="grey">{props.country}</span>
       </div>
-      <p>Life Lessons with Katie Zaferes</p>
-      <p><span className="bold">From $136</span> / person</p>
+      <p>{props.title}</p>
+      <p><span className="bold">{props.price}</span> / person</p>
     </div>
   );
 }
